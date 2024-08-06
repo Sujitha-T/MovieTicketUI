b33215b237b3729f9c5cc1701aadc46d0942ecbd
Sujitha T <216t.sujitha@gmail.com>
2024-08-06 22:20:26 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://movieticketbooking-fhbve2d3d0bngmh6.eastus-01.azurewebsites.net/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl:  'https://movieticketbooking-fhbve2d3d0bngmh6.eastus-01.azurewebsites.net/api/v1'
+};
